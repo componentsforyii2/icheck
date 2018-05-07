343f0bbb2c74df5d191b0189bde0eba3e5e5716c
helong <yuanshuai@mybns.cn>
2018-05-07 11:27:00 +0800

    init

---
+++ IcheckAssets.php
@@ -0,0 +1,17 @@
+<?php
+namespace componentsforyii2\icheck;
+use yii\web\AssetBundle;
+/**
+ * Class IcheckAssets
+ * @package yuanshuai\yscomponents\icheck
+ */
+class IcheckAssets extends AssetBundle {
+    public $sourcePath = "@vendor/bower/admin-lte/plugins/iCheck/";
+    public $js = [
+        'icheck.js'
+    ];
+    public $css = [
+        "all.css"
+    ];
+}
+?>
